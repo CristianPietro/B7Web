d5eb327044655c96ce185e215c8abfe1defec849
CristianPietro <crpietro26@gmail.com>
2022-12-06 09:35:01 -0300

    arrays

---
+++ Jacvascript/array/exercicio.js
@@ -0,0 +1,18 @@
+//1 - No Array abaixo, qual oo numero que pega a ferrari?
+let carros = ['BMW', 'Ferrari', 'Mercedes']
+let x = 1;
+ console.log('1. ' + carros[x])
+ 
+ //2 - Troque a ferrari por audi
+ carros = ['BMW', 'Audi', 'Mercedes']
+console.log('2. Lista com Audi:')
+console.log(carros)
+
+//3 - adcione volvo a lista
+carros.push("volvo")
+console.log('2. Lista com Volvo:')
+console.log(carros)
+
+//  4 - Exiba quantos itens tem no array
+
+console.log(carros.length)
---
+++ Jacvascript/array/operacao-array.js
@@ -0,0 +1,13 @@
+let ingredientes = [
+  'agua',
+  'farinha',
+  'ovo',
+  'sal',
+  'chocolate',
+  'granulado'
+];
+
+// ingredientes.pop();
+
+console.log(ingredientes.shift())
+console.log(`total de ingredientes: ${ingredientes.length}`)
