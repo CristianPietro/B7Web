3773f0fcb0595d2d7b31051d0fef16805caca5b2
CristianPietro <crpietro26@gmail.com>
2022-12-06 18:01:37 -0300

    interacao array

---
+++ Javascript/10-interacao-de-array/interacaoArray.js
@@ -0,0 +1,6 @@
+let fruits = ['banana', 'laranja', 'maça', 'pêra']
+// let bigFruits = fruits.filter((item) =>  item.length > 5);
+
+let big = fruits.every((value) => value.length > 2)
+console.log(big)
+
