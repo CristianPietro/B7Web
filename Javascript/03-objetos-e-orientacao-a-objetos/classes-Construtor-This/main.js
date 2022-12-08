f6b1b798085e4336b571ff3911894c46631b127b
CristianPietro <crpietro26@gmail.com>
2022-12-08 20:46:51 -0300

    classes: constructor e this

---
+++ Javascript/03-objetos-e-orientacao-a-objetos/classes-Construtor-This/main.js
@@ -0,0 +1,13 @@
+class Person {
+
+  age = 0;
+  constructor (name, age){
+     this.name = name;
+  }
+}
+
+p1.age = 20
+
+let p1 = new Person('João')
+let p2 = new Person('Maria')
+let p3 = new Person('Pedro')
