13d04e04c12efb33abd2f2f71dfcca4906eeea4d
CristianPietro <crpietro26@gmail.com>
2022-12-08 09:53:47 -0300

    manipulando atributos e css

--- Javascript/eventos-manipulacao-DOM/04-manipulando-atributos/main.js
+++ Javascript/eventos-manipulacao-DOM/04-manipulando-atributos/main.js
@@ -1,6 +1,15 @@
-function clicou(){
-  const input = document.querySelector('input')
+// funcao mostrar e ocultar senha
+
+function click(){
+  const input = document.querySelector('input');
+  const button = document.querySelector('.btn');
   
-  input.setAttribute('placeholder', 'alterado')
+  if(input.getAttribute('type') === 'text'){
+    input.setAttribute('type', "password");
+    button.innerText = 'Mostrar senha'
+  } else {
+    input.setAttribute('type', 'text');
+    button.innerText = 'Ocultar senha'
+  }
 
 }
