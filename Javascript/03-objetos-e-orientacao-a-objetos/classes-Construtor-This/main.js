4db04f2d88c15f5d69cd986789d4d50f86e9e4f1
CristianPietro <crpietro26@gmail.com>
2022-12-08 23:04:30 -0300

    classes: Action

--- Javascript/03-objetos-e-orientacao-a-objetos/classes-Construtor-This/main.js
+++ Javascript/03-objetos-e-orientacao-a-objetos/classes-Construtor-This/main.js
@@ -1,13 +1,32 @@
 class Person {
-
   age = 0;
-  constructor (name, age){
+  steps = 0;
+
+  constructor (name){
      this.name = name;
   }
+
+  takeStep(){
+    this.steps++;
+  }
+
+  setAge(newAge){
+    if(typeof newAge === 'number')
+    this.age = newAge;
+  }
+
 }
 
-p1.age = 20
+
 
 let p1 = new Person('João')
 let p2 = new Person('Maria')
 let p3 = new Person('Pedro')
+
+p1.setAge(25)
+
+console.log(`O ${p1.name} tem ${p1.age} anos`) 
+
+// console.log(`p1 = ${p1.name} tem ${p1.age} anos`);
+// console.log(`p2 = ${p2.name} tem ${p2.age} anos`);
+// console.log(`p3 = ${p3.name} tem ${p3.age} anos`);
