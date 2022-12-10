a132a40637c34156718359109da4075ef4549786
CristianPietro <crpietro26@gmail.com>
2022-12-10 15:41:11 -0300

    [2/3] projeto: criador de formularios

--- Javascript/03-objetos-e-orientacao-a-objetos/06-projeto-Criador_de_formularios/main.js
+++ Javascript/03-objetos-e-orientacao-a-objetos/06-projeto-Criador_de_formularios/main.js
@@ -1,3 +1,20 @@
+class Form{
+
+  items = []
+  method = 'GET';
+
+  constructor(container, method, post){
+    this.container = document.querySelector(container);
+    this.method = method;
+    this.post = post;
+  }
+
+addItem(item){
+  this.items.push(item)
+}
+
+}
+
 class Input{
 
   _type = 'text';
@@ -13,24 +30,38 @@ class Input{
   }
 
   set type(t){
-    if(['text', 'password', 'email', 'submit'].includes(t)){
+    if([ 'text', 'password', 'email', 'submit'].includes(t)){
       this._type = t;
     }else{
       throw new Error(`O input "${t}" don't exist`);
     }
   }
+
 }
 
-// IMLEMENTACAO
+class Button extends Input{
 
-//email
+constructor(label){
+  super(' ', label);
+  this.type = 'submit';
+}
+
+}
+
+let form = new Form('.formArea', 'POST',  'https://example.com');
+
 
 let email = new Input('email', 'Digite seu email');
 email.type = 'email';
 email.required = true;
+form.addItem(email)
 
-// password
 
-let password = new Input('password', 'Digite sua senha')
+let password = new Input('password', 'Digite sua senha');
 password.type = 'password';
-password.required = true
+password.required = true;
+form.addItem(password)
+
+let button = new Button('Enviar');
+form.addItem(button)
+
