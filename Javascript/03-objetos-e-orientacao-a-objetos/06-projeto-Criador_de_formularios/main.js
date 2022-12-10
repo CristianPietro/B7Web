6b6cef4a7855abb34ed0a766a5cca2fce9c6fb70
CristianPietro <crpietro26@gmail.com>
2022-12-10 14:05:26 -0300

    [1/3] cridor de formularios

--- Javascript/03-objetos-e-orientacao-a-objetos/06-projeto-Criador_de_formularios/main.js
+++ Javascript/03-objetos-e-orientacao-a-objetos/06-projeto-Criador_de_formularios/main.js
@@ -1,3 +1,36 @@
 class Input{
-  
+
+  _type = 'text';
+  required = false;
+
+  constructor(name, label){
+    this.name = name;
+    this.label = label;
+  }
+
+  get type(){
+    return this._type;
+  }
+
+  set type(t){
+    if(['text', 'password', 'email', 'submit'].includes(t)){
+      this._type = t;
+    }else{
+      throw new Error(`O input "${t}" don't exist`);
+    }
+  }
 }
+
+// IMLEMENTACAO
+
+//email
+
+let email = new Input('email', 'Digite seu email');
+email.type = 'email';
+email.required = true;
+
+// password
+
+let password = new Input('password', 'Digite sua senha')
+password.type = 'password';
+password.required = true
