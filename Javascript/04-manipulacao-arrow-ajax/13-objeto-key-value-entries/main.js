752a5ae07e4ac1cb40fdd3f73d458573d979012a
Cristian Pietro <crpietro26@gmail.com>
2022-12-16 16:40:43 -0300

    object: keys, values e entries

---
+++ Javascript/04-manipulacao-arrow-ajax/13-objeto-key-value-entries/main.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: 'Cristian',
+    sobrenome: 'Pietro',
+    idade: 26
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
