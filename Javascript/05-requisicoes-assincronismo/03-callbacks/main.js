fad050fc8c6ee753453aac8568f65beadb2b643a
Cristian Pietro <crpietro26@gmail.com>
2022-12-18 19:26:38 -0300

    callback

---
+++ Javascript/05-requisicoes-assincronismo/03-callbacks/main.js
@@ -0,0 +1,9 @@
+function alerta (){
+    console.log("Opa, tudo bem?")
+}
+
+
+setTimeout(alerta, 2000)
+let nome = 'Cristian';
+let sobrenome = 'Pietro';
+console.log(`NOME COMPLETO: ${nome} ${sobrenome}`)
