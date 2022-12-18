dad8efab0eaa301a9c51c2ab0319540ffd385dd2
Cristian Pietro <crpietro26@gmail.com>
2022-12-17 23:35:31 -0300

    sincrono vs assincrono

---
+++ Javascript/05-requisicoes-assincronismo/02-sincrono-vs-assincrono/main.js
@@ -0,0 +1,12 @@
+// SINCRONO
+
+let nome = "Cristian";
+let sobrenome = 'Pietro';
+let completo = nome + sobrenome;
+
+// ASSINCRONO
+
+let nome2 = "Cristian";
+let sobrenome2 = 'Pietro';
+let temperatura2 = maquininha.pegarTemperatura() // ASSINCRONO
+let completo2 = `${nome2} ${sobrenome2}`;
