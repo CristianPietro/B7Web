108043dba8b09166a18b8652f772a8c7227cd43e
Cristian Pietro <crpietro26@gmail.com>
2023-01-10 21:21:12 -0300

    nullish coalescing

---
+++ aulas-externas/nullish-operator-colector/index.js
@@ -0,0 +1,15 @@
+// function toggleMenu (toggle){
+//     const enable = toggle || true;
+//     return enable;
+// }
+
+// console.log(toggleMenu())
+
+
+const text1 = "";
+let text2;
+const text3 = 'casa';
+
+const teste = text2 ?? text3
+
+console.log(teste)
