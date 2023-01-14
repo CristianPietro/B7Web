bcaa4603c3876948f970814127748449816f65cb
Cristian Pietro <crpietro26@gmail.com>
2023-01-13 22:21:58 -0300

    prototype

--- aulas-externas/objetos/prototype.js
+++ aulas-externas/objetos/prototype.js
@@ -13,7 +13,31 @@ Microfone.prototype.toggleOnOff = () => {
 }
 
 
-const microfone = new Microfone();
-const microfone2 = new Microfone('white');
-console.log(microfone);
-console.log(microfone2);
+// const microfone = new Microfone();
+// const microfone2 = new Microfone('white');
+// console.log(microfone);
+// console.log(microfone2);
+
+const meuObjeto = {
+    a: 1,
+    b: 2,
+}
+
+const novoObjeto = Object.create(meuObjeto);
+
+novoObjeto.novaPropriedade = 5
+console.log(meuObjeto.hasOwnProperty('map'))
+
+
+
+//Meu objeto.__ptoro__ -> object.prototype -> null
+
+const meuArray = [1, 2, 3, 'a', 'b']
+
+//meuArray.__proto__ -> array.prototypec -> object.prototype - null
+
+function minhaFuncao(){
+    return 1 * 2
+}
+
+//minhaFuncao.__proto__ -> function.prototype -> object.prototype -> null
