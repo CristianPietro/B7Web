ac776c48580b1f7cd812e821590dca3973a184c2
Cristian Pietro <crpietro26@gmail.com>
2023-01-20 13:47:34 -0300

    instalando e entendendo typescript

--- Typescript/instalando-e-entendendo/instalando-e-usando-typescript/public/script.js
+++ Typescript/instalando-e-entendendo/instalando-e-usando-typescript/public/script.js
@@ -3,15 +3,8 @@ var numero2 = document.getElementById('numero2');
 var botao = document.getElementById('calcular');
 var res = document.getElementById('resultado');
 function calcular(n1, n2) {
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1 + n2;
-    }
-    else {
-        n1 = parseFloat(n1);
-        n2 = parseFloat(n2);
-        return n1 + n2;
-    }
+    return n1 + n2;
 }
 botao.addEventListener('click', function () {
-    res.innerHTML = calcular(numero1.value, numero2.value);
+    res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
 });
--- Typescript/instalando-e-entendendo/instalando-e-usando-typescript/src/script.ts
+++ Typescript/instalando-e-entendendo/instalando-e-usando-typescript/src/script.ts
@@ -3,16 +3,10 @@ let numero2 = document.getElementById('numero2') as HTMLInputElement;
 let botao = document.getElementById('calcular');
 let res = document.getElementById('resultado');
 
-function calcular(n1, n2){
-    if(typeof n1 === 'number' && typeof n2 === 'number'){
-        return n1 + n2;
-    }else{
-        n1 = parseFloat(n1);
-        n2 = parseFloat(n2);
-        return n1 + n2;
-    }
+function calcular(n1: number, n2: number){
+    return n1 + n2;
 }
 
 botao.addEventListener('click', () =>{
-    res.innerHTML = calcular(numero1.value, numero2.value)
+    res.innerHTML = calcular( +numero1.value, +numero2.value ).toString();
 });
